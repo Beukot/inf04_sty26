1c2b47a4e5cccc6181f0726230862a0bb3a4123b
Beukot <michalek7790@gmail.com>
2026-01-19 22:32:01 +0100

    added notes about how what works

--- app/src/main/java/com/example/inf04_sty26/MainActivity.java
+++ app/src/main/java/com/example/inf04_sty26/MainActivity.java
@@ -15,6 +15,11 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    //
+    // Aha, w xml jak masz obrazki to daj im adjustViewBounds na true żeby miały poprawny rozmiar
+    //
+
+
     public Dice[] dices;
     public Button rollButton;
     public TextView rollResultText;
@@ -31,7 +36,10 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        // tutaj zrobiłem se tablice na kości. sowwy że zrobiłem klasę ale to nie jest skomplikowane :3
+        // inaczej bym musiał zrobić kilka tabelek :P
         dices = new Dice[] {
+                // tutaj zresztą masz findviewbyid tylko przy tworzeniu obiektu
                 new Dice(findViewById(R.id.dice1), 0, true),
                 new Dice(findViewById(R.id.dice2), 0, true),
                 new Dice(findViewById(R.id.dice3), 0, true),
@@ -44,13 +52,15 @@ public class MainActivity extends AppCompatActivity {
 
         rollsSum = 0;
 
+        // tu jest pętla co każdemu z imageView w tablicy daje funkcję onclick.
         for (int i = 0; i < dices.length; i++) {
-            int finalI = i;
+            int finalI = i;       // to ci powinno podpowiedzieć, no ale poniżej masz sus funkcję co nie możesz przyjąć zmiennej "i" z pętli for
+
             dices[i].diceImageView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    if (dices[finalI].diceAvailability) {
-                        view.setAlpha(0.5F);
+                    if (dices[finalI].diceAvailability) { // btw jak nie załapałeś to ten if to to samo co if (dices[finalI].diceAvailability == true)
+                        view.setAlpha(0.5F); // to jest inaczej połowa przezroczystości (to F ci też raczej podpowie, oznacza on że  to jest float bo normalnie uznaje to za double)
                         dices[finalI].diceAvailability = false;
                     } else {
                         view.setAlpha(1);
@@ -67,7 +77,9 @@ public class MainActivity extends AppCompatActivity {
 
                 for (int i = 0; i < dices.length; i++) {
                     if (dices[i].diceAvailability) {
+                        // a no i musisz generator liczb losowych stworzyć :P
                         Random rand = new Random();
+                        // w javie nextInt losuje ci int od 0 włącznie do podanej wartości bez niej (dlatego +1 jest)
                         int rollResult = rand.nextInt(6) + 1;
 
                         dices[i].setDiceValue(rollResult);
@@ -77,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
 
+                // pamiętaj żeby zamieniać inty na stringi! :3
                 rollResultText.setText(Integer.toString(rollsSum));
             }
         });
